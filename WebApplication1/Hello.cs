e9d064afa18ec1db129907484b588e718d656a2f
Shawn Weisfeld <shawn@shawnweisfeld.com>
2014-07-24 14:32:07 -0500

    init

---
+++ WebApplication1/Hello.cs
@@ -0,0 +1,28 @@
+﻿using ServiceStack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    [Route("/hello")]
+    [Route("/hello/{Name}")]
+    public class Hello
+    {
+        public string Name { get; set; }
+    }
+
+    public class HelloResponse
+    {
+        public string Result { get; set; }
+    }
+
+    public class HelloService : Service
+    {
+        public object Any(Hello request)
+        {
+            return new HelloResponse { Result = "Hello, " + request.Name };
+        }
+    }
+}
